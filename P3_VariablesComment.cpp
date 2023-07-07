6c6700505ddeb5e1f39781bf57e6b62c1b317df3
up81harsh <119611563+up81harsh@users.noreply.github.com>
2023-07-08 00:25:47 +0530

    Commit

---
+++ P3_VariablesComment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a=20, b=30;
+	cout<<"ADDITION\n20 + 30 = "<<a+b;
+	cout<<"\nMULTIPLICATION\n20 x 30 = "<<a*b;
+	cout<<"\nSUBTRACTION\n20 - 30 = "<<a-b;
+	cout<<"\nDIVISION\n20 / 30 = "<<a/b;
+	return 0;
+//	Comment
+/* MUltiline
+comment*/
+}
